69dd2ef2c55c215332e21f802b1b2210a274d53e
NinaHwang <hwang.ninaa@gmail.com>
2021-03-22 22:23:24 +0900

    Section 6 + Section 7
        - Section6: Webpack
        - Section7: Using a Third-Party Component

---
+++ indecision-app/webpack.config.js
@@ -0,0 +1,26 @@
+// entry point -> output
+const path = require('path');
+/*  to find the path....
+    console.log(path.join(__dirname, 'public'))
+    (in terminal) node webpack.config.js */
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+};
